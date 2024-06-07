fb8cd47d7b70d5eaa5549c61031493393786349a
Darshan Siddalingappa <darshan.sid@gmail.com>
2024-06-07 18:03:47 +0530

    Initial Commit

---
+++ src/main/java/com/ds/learning/java_spring_maven_docker_actions/JavaSpringMavenDockerActionsApplication.java
@@ -0,0 +1,13 @@
+package com.ds.learning.java_spring_maven_docker_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMavenDockerActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMavenDockerActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/learning/java_spring_maven_docker_actions/JavaSpringMavenDockerActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.learning.java_spring_maven_docker_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMavenDockerActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
